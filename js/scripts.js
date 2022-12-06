b31e7d6b8b052f496124aabbcef77e11c7aafe97
Hans-Bachman <hans.lovestanks@gmail.com>
2022-12-05 20:08:39 -0700

    got basic logic working

--- js/scripts.js
+++ js/scripts.js
@@ -1,79 +1,56 @@
-// Images for head
-var characterImagesHeads = ["./imgs/head1.png", "./imgs/head2.png", "./imgs/head3.png"];
-
-// Images for the body
-var characterImagesBody = ["./imgs/body1.png", "./imgs/body2.png", "./imgs/body3.png"];
-
-// Images for the feet
-var characterImagesFeet = ["./imgs/foot1.png", "./imgs/foot2.png", "./imgs/foot3.png"];
+// inputs
+var input = "";
+var aiInput = 0;
 
 
-//Background Colours
-var colours = ["#301A4B", "#3D5A80"];
-
-//Values to store current displayed image
-var headIndex = 0;
-var bodyIndex = 0;
-var feetIndex = 0;
-var currentColour = 0;
-
-//Checking for clicks to update the images
-$("#head").on("click", updateHead);
-$("#body").on("click", updateBody);
-$("#feet").on("click", updateFeet);
-
-//Functions to update respective images
-function updateHead() {
-    headIndex++;
-    if (headIndex >= characterImagesHeads.length){
-        headIndex = 0;
-    };
-
-    $("#head").html("<img src= " + characterImagesHeads[headIndex] + " alt='HeadImage'></img>");
+// get user value
+function buttonPress(hotsex) {
+    switch(hotsex) {
+    case "Rock":
+        input = 0;
+        break;
+    case "Paper":
+        input = 1;
+        break;
+    case "Scissors":
+        input = 2;
+        break;
+    } 
 };
 
-function updateBody() {
-    bodyIndex++;
-    if (bodyIndex >= characterImagesBody.length){
-        bodyIndex = 0;
-    };
-
-    $("#body").html("<img src= " + characterImagesBody[bodyIndex] + " alt='Body Image'></img>");
+// gen counter value
+function randomInput() {
+    aiInput = Math.floor(Math.random() * 3);
 };
 
-function updateFeet() {
-    feetIndex++;
-    if (feetIndex >= characterImagesFeet.length){
-        feetIndex = 0;
-    };
+// compare values 
+function fight() {
+    var answer = input - aiInput;
 
-    $("#feet").html("<img src= " + characterImagesFeet[feetIndex] + " alt='Foot Image'></img>");
-};
-
-//random character with keypress
-$(document).on("keypress", function(one){
-    if (one.key == "1"){
-        //getting random values for arrays
-        headIndex = Math.floor(Math.random() * characterImagesHeads.length);
-        bodyIndex = Math.floor(Math.random() * characterImagesBody.length);
-        feetIndex = Math.floor(Math.random() * characterImagesFeet.length);
-
-        //updating the images
-        updateHead();
-        updateBody();
-        updateFeet();
+    if (answer == 0) {
+     //Tie
+     console.log(input);
+     console.log(aiInput);
+     console.log("tie");
     }
-});
-
-
-//Changing theme with keypress
-$(document).on("keypress", function(two){
-    if (two.key == "2"){
-        currentColour++;
-        if (currentColour >= colours.length){
-            currentColour = 0;
-        };
+    else if (answer == input || answer == -2) {
+        //win
+        console.log(input);
+        console.log(aiInput);
+        console.log("win");
     }
+    else {
+        //lose
+        console.log(input);
+        console.log(aiInput);
+        console.log("lose");
+    }
+    
+};
 
-    $(".wrap").css("background-color", colours[currentColour]);
+$(document).on("keypress", function(enter){
+    if (enter.key == "Enter"){
+        randomInput();
+        fight();
+    }
 });
